ce2b2081cfd24a32f9c32e3f8eaaee03420da119
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-24 16:11:02 -0400

    setting up linkedlist

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,22 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import javax.xml.soap.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    private Node head;
+    private int listCount;
+
+    public SinglyLinkedList() {
+        head = new Node(null);
+        listCount = 0;
+    }
+
+    public void add(Object data) {
+        Node tmp = new Node(data);
+    }
+
 }
