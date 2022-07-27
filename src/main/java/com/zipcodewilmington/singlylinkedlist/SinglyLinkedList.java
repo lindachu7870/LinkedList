8a4860716e6f094c510afe706171d42e86351921
Meilin Li (Linda) <limeilinda@gmail.com>
2022-07-27 10:00:36 -0400

    WIP

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,38 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.w3c.dom.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    private static Node[] singlyLinkedList = new Node[0];
+
+    public SinglyLinkedList(Node[] nodeArr) {
+        singlyLinkedList = nodeArr;
+    }
+
+    public SinglyLinkedList() {
+    }
+
+    public Node get(Integer point) {
+        return singlyLinkedList[point];
+    }
+
+    public void add(Node node) {
+        Node [] addArrList = new Node[singlyLinkedList.length + 1];
+        System.arraycopy(singlyLinkedList, 0, addArrList, 0, singlyLinkedList.length);
+        addArrList[singlyLinkedList.length] = node;
+        singlyLinkedList= addArrList;
+    }
+
+    public boolean contains(Node node) {
+        for (Node n : singlyLinkedList) {
+            if (n == node) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.w3c.dom.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addTest() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        Node node = new Node();
+
+        singlyLinkedList.add(node);
+
+        Assert.assertTrue(singlyLinkedList.contains(node));
+    }
 }
